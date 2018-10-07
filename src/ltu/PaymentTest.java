2cd9c05341fc42a4fe391cca20125447070b14e8
linwil-5 <linwil-5@student.ltu.se>
2018-10-08 00:49:01 +0200

    Inital commit

---
+++ src/ltu/PaymentTest.java
@@ -0,0 +1,16 @@
+package ltu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PaymentTest
+{
+	
+    @Test
+    public void testSilly()
+    {
+        assertEquals(1, 1);
+    }
+	
+}
