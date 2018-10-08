7085e78e1eba3e52da9cd05b7ca7e8ddb839cd20
linwil-5 <linwil-5@student.ltu.se>
2018-10-08 17:34:45 +0200

    Inital commit

--- src/ltu/PaymentTest.java
+++ src/ltu/PaymentTest.java
@@ -20,19 +20,6 @@ import ltu.CalendarFactory;
 public class PaymentTest
 {
 
-    @Test	// Test if person with 0 income can get full load if all other is correct
-    public void test_zero_income()	throws IOException	//ID: 301
-    {
-		ICalendar calend = CalendarFactory.getCalendar();
-		PaymentImpl payment = new PaymentImpl(calend);			
-		
-		int Expected_Full_Loan = (7088 + 2816);
-
-		// The student is under 47 years of age and has 100% study and completion rate and 0 income.
-		assertEquals(Expected_Full_Loan, payment.getMonthlyAmount("19951124-0000", 0, 100, 100));
-	}
-
-	
 	@Test	// [ID: 101] The student must be at least 20 years old to receive subsidiary and student loans.
 	public void test_Under_20_Years_Old() throws IOException //ID: 101
 	{
@@ -40,7 +27,7 @@ public class PaymentTest
 		PaymentImpl payment = new PaymentImpl(calend);			
 		
 		int Expected_Zero_Loan = (0);
-		assertEquals(Expected_Zero_Loan, payment.getMonthlyAmount("19970101-0000", 0, 100, 100));
+		assertEquals(Expected_Zero_Loan, payment.getMonthlyAmount("19990101-0000", 0, 100, 100));
 	}
 
 	
@@ -58,12 +45,22 @@ public class PaymentTest
 	@Test
 	public void test_47_Years_Old() throws IOException //ID: 103
 	{
+		ICalendar calend = CalendarFactory.get Calendar();
+		PaymentImpl payment = new PaymentImpl(calend);			
+		
+		int Expected_Subsidiary = (2816);
+		// The student is 47 years of age and has 100% study and completion rate and 0 income.
+		assertEquals(Expected_Zero_Loan, payment.getMonthlyAmount("19690101-0000", 0, 100, 100));
+	}
+	
+	@Test	// Test if person with 0 income can get full load if all other is correct
+    public void test_zero_income()	throws IOException	//ID: 301
+    {
 		ICalendar calend = CalendarFactory.getCalendar();
 		PaymentImpl payment = new PaymentImpl(calend);			
 		
-		int Expected_Zero_Loan = (2816);
-		// The student is 47 years of age and has 100% study and completion rate and 0 income.
-		assertEquals(Expected_Zero_Loan, payment.getMonthlyAmount("19690101-0000", 0, 100, 100));
+		int Expected_Full_Loan = (7088 + 2816);
+		assertEquals(Expected_Full_Loan, payment.getMonthlyAmount("19951124-0000", 0, 100, 100));
 	}
 
 }
