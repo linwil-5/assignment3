1107f202b5ec34dfa860ad234cbe394333a94acb
linwil-5 <linwil-5@student.ltu.se>
2018-10-08 13:25:03 +0200

    Implemented non working methods

--- src/ltu/PaymentTest.java
+++ src/ltu/PaymentTest.java
@@ -1,16 +1,52 @@
 package ltu;
 
-import static org.junit.Assert.*;
+import static ltu.CalendarFactory.getCalendar;
+import static java.lang.Integer.parseInt;
 
 import org.junit.Test;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Properties;
+import ltu.PaymentImpl;
+import ltu.Main;
+import ltu.IPayment;
+import ltu.ICalendar;
+import ltu.CalendarImpl;
+import ltu.CalendarFactory;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 public class PaymentTest
 {
+	// 			Instances Created here 			//
+	ICalendar calend = CalendarFactory.getCalendar();
 	
     @Test
     public void testSilly()
     {
+		
         assertEquals(1, 1);
+    }
+	
+    @Test
+    public void test_if_below_max_income_for_fullLoanStudent() //ID: 301
+    {
+		int excpected = (7088 + 2816); 
+		
+		assertEquals(1, 1);
+    }
+	
+	@Test
+    public void test_if_below_max_income_for_halfLoanStudent() //ID: 301
+    {
+		int excpected = (7088 + 2816); 
+		
+		assertEquals(1, 1);
     }
 	
 }
