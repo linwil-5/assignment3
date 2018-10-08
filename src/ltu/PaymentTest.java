2fd039f4b58c43bc1ffe067041e8eee66b75b16e
linwil-5 <linwil-5@student.ltu.se>
2018-10-08 17:59:55 +0200

    Removed unnecessarry code

--- src/ltu/PaymentTest.java
+++ src/ltu/PaymentTest.java
@@ -19,25 +19,26 @@ import ltu.CalendarFactory;
 
 public class PaymentTest
 {
+	ICalendar calend = CalendarFactory.getCalendar();
 
-
+	
 	@Test	// [ID: 101] The student must be at least 20 years old to receive subsidiary and student loans.
 	public void test_Under_20_Years_Old() throws IOException //ID: 101
-	{
-		ICalendar calend = CalendarFactory.getCalendar();
-		PaymentImpl payment = new PaymentImpl(calend);			
-		
+	{		
+		PaymentImpl payment = new PaymentImpl(calend);
+
 		int Expected_Zero_Loan = (0);
 		assertEquals(Expected_Zero_Loan, payment.getMonthlyAmount("19980101-0000", 0, 100, 100));
 
 	}
 	
+	
 	@Test	// [ID: 102] The student may receive subsidiary until the year they turn 56.
 	public void test_56_Years_Old() throws IOException //ID: 102
-	{
-		ICalendar calend = CalendarFactory.getCalendar();
-		PaymentImpl payment = new PaymentImpl(calend);			
-		
+	{		
+		PaymentImpl payment = new PaymentImpl(calend);
+
+			
 		int Expected_Zero = (0);
 		assertEquals(Expected_Zero, payment.getMonthlyAmount("19600101-0000", 0, 100, 100));
 	}
@@ -45,20 +46,27 @@ public class PaymentTest
 
 	@Test	// [ID: 103] The student may not receive any student loans from the year they turn 47.
 	public void test_47_Years_Old() throws IOException //ID: 103
-	{
-		ICalendar calend = CalendarFactory.getCalendar();
-		PaymentImpl payment = new PaymentImpl(calend);			
+	{	
+		PaymentImpl payment = new PaymentImpl(calend);
 
+	
 		int Expected_Subsidary = (2816);
 		assertEquals(Expected_Subsidary, payment.getMonthlyAmount("19690101-0000", 0, 100, 100));
 	}
 
+	
+	
+	
+
+	
+	
+	
 	@Test	// Test if person with 0 income can get full load if all other is correct
     public void test_zero_income()	throws IOException	//ID: 301
-    {
-		ICalendar calend = CalendarFactory.getCalendar();
-		PaymentImpl payment = new PaymentImpl(calend);			
-		
+    {	
+		PaymentImpl payment = new PaymentImpl(calend);
+
+	
 		int Expected_Full_Loan = (7088 + 2816);
 		assertEquals(Expected_Full_Loan, payment.getMonthlyAmount("19951124-0000", 0, 100, 100));
 	}
