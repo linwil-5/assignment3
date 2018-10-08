6a07d101740dc2f26da123f3130cef1993746b54
linwil-5 <linwil-5@student.ltu.se>
2018-10-08 17:21:24 +0200

    This is how it should be

--- src/ltu/PaymentTest.java
+++ src/ltu/PaymentTest.java
@@ -27,13 +27,14 @@ public class PaymentTest
 		PaymentImpl payment = new PaymentImpl(calend);			
 		
 		int Expected_Full_Loan = (7088 + 2816);
-<<<<<<< HEAD
+
 		// The student is under 47 years of age and has 100% study and completion rate and 0 income.
 		assertEquals(Expected_Full_Loan, payment.getMonthlyAmount("19951124-0000", 0, 100, 100));
 	}
 
+	
 	@Test	// [ID: 101] The student must be at least 20 years old to receive subsidiary and student loans.
-	public void testIfUnder20YearsOld() throws IOException //ID: 101
+	public void test_Under_20_Years_Old() throws IOException //ID: 101
 	{
 		ICalendar calend = CalendarFactory.getCalendar();
 		PaymentImpl payment = new PaymentImpl(calend);			
@@ -42,8 +43,9 @@ public class PaymentTest
 		assertEquals(Expected_Zero_Loan, payment.getMonthlyAmount("19970101-0000", 0, 100, 100));
 	}
 
+	
 	@Test	// [ID: 102] The student may receive subsidiary until the year they turn 56.
-	public void testIf56YearsOld() throws IOException //ID: 102
+	public void test_56_Years_Old() throws IOException //ID: 102
 	{
 		ICalendar calend = CalendarFactory.getCalendar();
 		PaymentImpl payment = new PaymentImpl(calend);			
@@ -52,15 +54,9 @@ public class PaymentTest
 		assertEquals(Expected_Zero_Loan, payment.getMonthlyAmount("19600101-0000", 0, 100, 100));
 	}
 
-	@Test	// The student may not receive any student loans from the year they turn 47.
-=======
-		assertEquals(Expected_Full_Loan, payment.getMonthlyAmount("1995112400000", 0, 100, 100));
-	}
-
 
 	@Test
->>>>>>> 05cb6490f6d520c5adfb375f5e44c5d75bd35be8
-	public void testIf47YearsOld() throws IOException //ID: 103
+	public void test_47_Years_Old() throws IOException //ID: 103
 	{
 		ICalendar calend = CalendarFactory.getCalendar();
 		PaymentImpl payment = new PaymentImpl(calend);			
